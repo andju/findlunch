f556f35dc5fb27f7d038b650b46aec58d0b4c04f
Sergej Bardin <sergej_bardin@yahoo.de>
2017-04-26 13:54:12 +0200

    Web and Android Push implemented
    - Corrected Firebase Project
    - Web and Android Push implemented

--- ionicApp/src/pages/home/home.ts
+++ ionicApp/src/pages/home/home.ts
@@ -11,4 +11,64 @@ export class HomePage {
 
   }
 
+  sendToWeb() {
+    // Add Serverkey and Webtoken here
+    var webKey = 'SERVERKEY-API';
+    var toWeb = 'WEB-TOKEN';
+
+    let notification = {
+      'title': 'Bestellung XY eingegangen',
+      'body': 'Kunde: Maximilian Mustermann',
+      'icon': '/assets/icon/favicon.ico',
+      'click_action': 'http://localhost:8100'
+    };
+
+    fetch('https://fcm.googleapis.com/fcm/send', {
+      'method': 'POST',
+      'headers': {
+        'Authorization': 'key=' + webKey,
+        'Content-Type': 'application/json'
+      },
+      'body': JSON.stringify({
+        'notification': notification,
+        'to': toWeb
+      })
+    }).then(function(response) {
+      console.log(response);
+    }).catch(function(error) {
+      console.error(error);
+    })
+
+  }
+
+  sendToAndroid() {
+    // Add Serverkey and Android Token
+    var androidKey = 'SERVERKEY-API';
+    var toAndroid = 'ANDROID-TOKEN';
+
+    let notification = {
+      'title': 'Bestätigung der Bestellung XY',
+      'body': 'Abholbereit in ca. 20 min',
+      'icon': 'firebase-logo.png',
+      'click_action': 'http://localhost:8100'
+    };
+
+    fetch('https://fcm.googleapis.com/fcm/send', {
+      'method': 'POST',
+      'headers': {
+        'Authorization': 'key=' + androidKey,
+        'Content-Type': 'application/json'
+      },
+      'body': JSON.stringify({
+        'notification': notification,
+        'to': toAndroid
+      })
+    }).then(function(response) {
+      console.log(response);
+    }).catch(function(error) {
+      console.error(error);
+    })
+
+  }
+
 }
